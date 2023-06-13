e9fcec206a2d279af49eb05b752ea1fdea6bdd27
Coow <eirik_skarding@hotmail.no>
2023-06-13 18:03:51 +0200

    Add Swap Casters button

--- src/dashboard/CasterManager.tsx
+++ src/dashboard/CasterManager.tsx
@@ -13,11 +13,22 @@ export function CasterManager() {
 		set_caster2(event.target.caster2.value)
 	}
 
+	const swapTeams = (event: any) => {
+		event.preventDefault();
+		console.log("Swapping casters around")
+		let temp = caster1
+		set_caster1(caster2)
+		set_caster2(temp)
+	}
+
 	return (
 		<>
 			<form onSubmit={handleSubmit}>
 				<label>Caster 1</label>
 				<input type="text" placeholder="Caster1" name="caster1" defaultValue={caster1} />
+				<button onClick={swapTeams} className="swapButton w-36" name="swapTeams">
+					Swap Casters
+				</button>
 				<label>Caster 2</label>
 				<input type="text" placeholder="Caster2" name="caster2" defaultValue={caster2} />
 
