55f38a3cb1f79d979ff4e28168814f9155a557ca
Coow <eirik_skarding@hotmail.no>
2023-09-22 03:57:10 +0200

    Changed response to be more dynamic, depending on settings

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -4,16 +4,22 @@ module.exports = function (nodecg: NodeCG.ServerAPI) {
     const caster1 = nodecg.Replicant<string>('caster1');
 	const caster2 = nodecg.Replicant<string>('caster2');
 	const observer = nodecg.Replicant<string>('observer');
+	const showobserver = nodecg.Replicant<boolean>('showobserver');
 
     const router = nodecg.Router();
 
     router.get('/casters', (req, res) => {
-		res.send(`Todays casters: Left: ${caster1.value} & Right: ${caster2.value}! Observer: ${observer.value}`);
+        let response = `Todays casters: Left: ${caster1.value} & Right: ${caster2.value}!`
+        {showobserver.value ? response += `Observer: ${observer.value}` : ''}
+		res.send(response);
 	});
 
     
     router.get('/observer', (req, res) => {
-		res.send(`Todays Observer: ${observer.value}`);
+        let response = ''
+        {showobserver.value ? response = `Todays Observer: ${observer.value}` : response = 'No dedicated observer today :('}
+        
+		res.send(response);
 	});
 
     nodecg.mount('/caster', router);
